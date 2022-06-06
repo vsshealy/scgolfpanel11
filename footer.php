f88423cc91eb845550ea56f985270eb82f1b7c85
Scott Shealy <vsshealy@gmail.com>
2022-06-06 13:01:46 -0400

    Added custom single template file

--- footer.php
+++ footer.php
@@ -15,9 +15,9 @@
 
             if (! function_exists('elementor_theme_do_location') || ! elementor_theme_do_location('footer')) {
                 if (did_action('elementor/loaded') && hello_header_footer_experiment_active()) {
-                    get_template_part('php/theme/templates/dynamic-footer');
+                    get_template_part('template-parts/dynamic-footer');
                 } else {
-                    get_template_part('php/theme/templates/footer');
+                    get_template_part('template-parts/footer');
                 }
             }
         ?>
---
+++ template-parts/dynamic-header.php
@@ -0,0 +1,12 @@
+<?php 
+    /**
+     * php/theme/templates/dynamic-header.php
+     * @package scgolfpanel
+     * @author Scott Shealy
+     * @version 1.0.0
+     * @copyright 2022
+    **/
+?>
+
+<!-- #HEADER-MAIN -->
+<header id="header-main"></header>
